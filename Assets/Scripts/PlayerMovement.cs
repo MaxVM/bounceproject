3a7ba159e748f960edc15cb7f21164babb5e548e
MaxVM <54801082+MaxVM@users.noreply.github.com>
2020-02-28 15:25:43 +0100

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public float speed = 10.0F;
+    public float jumpspeed = 20.0F;
+    public float mfallSpeed = 20.0F;
+    public float gravity = 10.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                if (controller.isGrounded)
+                {
+                    moveDirection.y = jumpspeed;
+                }
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime * 5.5F;
+        if (moveDirection.y == mfallSpeed)
+        {
+            moveDirection.y = mfallSpeed;
+        }
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.Audio.AudioMixerGroup _audioMixer;
+    [SerializeField]
+    private AudioClip _audioClip;
+    [SerializeField]
+    private UnityEngine.Audio.AudioMixerGroup nul;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<AudioSource>())
+        {
+            other.GetComponent<AudioSource>().outputAudioMixerGroup = _audioMixer;
+            other.GetComponent<AudioSource>().clip = _audioClip;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<AudioSource>())
+        {
+            other.GetComponent<AudioSource>().outputAudioMixerGroup = nul;
+        }
+    }
+}
